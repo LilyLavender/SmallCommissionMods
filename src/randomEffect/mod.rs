ddc81d07d6b75168b2a0f24f0a90283af745151d
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2023-12-11 20:48:05 -0600

    Updated randomEffect to make death much less common

--- src/randomEffect/mod.rs
+++ src/randomEffect/mod.rs
@@ -1,52 +1,61 @@
-use smash::hash40;
-use smash::phx::Hash40;
-use smash::phx::Vector3f;
-use smash::lib::lua_const::*;
-use smash::lib::*;
-use smash::app::*;
-use smash::app::lua_bind::*;
-use smash::lua2cpp::L2CAgentBase;
-use smash::lua2cpp::{L2CFighterCommon};
-use smashline::*;
-use smash_script::*;
+use {
+    smash::{
+        lua2cpp::*,
+        phx::*,
+        app::{sv_animcmd::*, lua_bind::*, *},
+        lib::{lua_const::*, L2CValue, L2CAgent},
+		hash40
+    },
+    smash_script::*,
+	smashline::*
+};
 
 #[skyline::hook(replace = sv_animcmd::ATTACK)]
 unsafe fn attack_replace(lua_state: u64) {
-    // let boma = smash::app::sv_system::battle_object_module_accessor(lua_state);
-    // let fighter_kind = smash::app::utility::get_kind(boma);
     let mut l2c_agent = L2CAgent::new(lua_state);
     let hitbox_params: Vec<L2CValue> = (0..36).map(|i| l2c_agent.pop_lua_stack(i + 1)).collect();
     l2c_agent.clear_lua_stack();
     for i in 0..36 {
         if i == 32 {
-            let randomEffect = sv_math::rand(hash40("fighter"), 23) as usize;
-			let randomTxt = [
-				82430076406,
-				84299446750,
-				82425287246,
-				84607901136,
-				116764182975,
-				88148215354,
-				87856530745,
-				82781917156,
-				84803683138,
-				90808232845,
-				80658106433,
-				81191629927,
-				88498338105,
-				92925133491,
-				101406614725,
-				89104769103,
-				91968837809,
-				88531030715,
-				82063051125,
-				82443485437,
-				87514974371,
-				84931485642,
-				87658628301
+			let randomEffectType = sv_math::rand(hash40("fighter"), 1000) as usize;
+			let colAttrVisual = [
+				hash40("collision_attr_aura"),
+				hash40("collision_attr_coin"),
+				hash40("collision_attr_cutup"),
+				hash40("collision_attr_elec"),
+				hash40("collision_attr_fire"),
+				hash40("collision_attr_magic"),
+				hash40("collision_attr_normal"),
+				hash40("collision_attr_pierce"),
+				hash40("collision_attr_punch"),
+				hash40("collision_attr_purple"),
+				hash40("collision_attr_rush"),
+				hash40("collision_attr_stab"),
+				hash40("collision_attr_sting"),
+				hash40("collision_attr_water"),
+				hash40("collision_attr_whip")
 			];
-			let chosen = randomTxt[randomEffect];
-            l2c_agent.push_lua_stack(&mut L2CValue::new_int(chosen));
+			let colAttrSpecial = [
+				hash40("collision_attr_bind"),
+				hash40("collision_attr_bury"),
+				hash40("collision_attr_curse_poison"),
+				hash40("collision_attr_flower"),
+				hash40("collision_attr_ice"),
+				hash40("collision_attr_lay"),
+				hash40("collision_attr_paralyze"),
+				hash40("collision_attr_sleep"),
+				hash40("collision_attr_slip"),
+				hash40("collision_attr_turn")
+			];
+			
+			let chosenEffect = if randomEffectType < 400 {
+				colAttrVisual[sv_math::rand(hash40("fighter"), colAttrVisual.len() as i32) as usize]
+			} else if randomEffectType > 400 {
+				colAttrSpecial[sv_math::rand(hash40("fighter"), colAttrSpecial.len() as i32) as usize]
+			} else {
+				hash40("collision_attr_death")
+			};
+            l2c_agent.push_lua_stack(&mut L2CValue::new_int(chosenEffect));
         }
         else {
             l2c_agent.push_lua_stack(&mut hitbox_params[i].clone());
