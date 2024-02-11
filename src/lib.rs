82cd9f6496f09d3d341d0fbf4e6a62007cc29e6c
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2024-02-10 18:53:48 -0600

    Added comments regarding FighterManager

--- src/lib.rs
+++ src/lib.rs
@@ -5,23 +5,24 @@
 #![allow(
     unused_imports,
     unused_parens,
-	unused_macros,
-	unused_variables,
-	unused_assignments,
-	non_upper_case_globals,
-	non_snake_case,
-	dead_code,
+    unused_macros,
+    unused_variables,
+    unused_assignments,
+    non_upper_case_globals,
+    non_snake_case,
+    dead_code,
     clippy::borrow_interior_mutable_const
 )]
 
-// add the mod you want to build here
+// Add the mod you want to build here
 
+// Only used in nerfPacman & nerfGameWatch for keeping them in place
 extern "C"{
     #[link_name = "_ZN3lib9SingletonIN3app14FighterManagerEE9instance_E"]
     pub static FIGHTER_MANAGER: *mut smash::app::FighterManager;
 }
 
-#[skyline::main(name = "randomEffect")]
+#[skyline::main(name = "smashline_test")]
 pub fn main() {
-    // add the mod you want to build here
+    // Add the mod you want to build here
 }
